25e88b6de7831120dc8bb8fe50e2ffc3e36df9fa
rishi <rishikesh2715@gmail.com>
2023-08-29 23:07:59 -0500

    init commit

---
+++ uartecho_MSP_EXP432E401Y_nortos_ccs/Debug/uartecho.d
@@ -0,0 +1,63 @@
+# FIXED
+
+uartecho.obj: ../uartecho.c
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+uartecho.obj: D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+uartecho.obj: C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/drivers/GPIO.h
+uartecho.obj: C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/drivers/UART.h
+uartecho.obj: syscfg/ti_drivers_config.h
+uartecho.obj: C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/devices/DeviceFamily.h
+
+../uartecho.c:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+D:/code\ composer/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/drivers/GPIO.h:
+
+C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/drivers/UART.h:
+
+syscfg/ti_drivers_config.h:
+
+C:/ti/simplelink_msp432e4_sdk_4_20_00_12/source/ti/devices/DeviceFamily.h:
+
